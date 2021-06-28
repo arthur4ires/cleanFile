fe8930ec78a6a156d96d54993b207d52e38095d5
Arthur A <aires.arthur.15@gmail.com>
2021-06-27 22:01:57 -0300

    Update clean_txt.py

--- clean_txt.py
+++ clean_txt.py
@@ -13,10 +13,32 @@ def cleanFile(argumentosScript):
 
 	if '|' not in argumentosScript.strings:
 		print('Set | in strings args.')
+		exit()
+
+	duplicateUrls = []
 
 	for a in arquivoClean:
+
+		if argumentosScript.extensions != None:
+
+			if '|' not in argumentosScript.extensions:
+				print('Set | in extensions args.')
+				exit()
+
+			if '.' in a:
+
+				stopExtension = False
+
+				for _ in argumentosScript.extensions.split('|'):
+					if _ in a.split('.')[-1].replace('\n',''):
+							stopExtension = True
+							break
+
+				if stopExtension == True:
+					continue
+
 		keywordNot = False
-		
+
 		for b in argumentosScript.strings.split('|'):
 
 			if b in a:
@@ -33,6 +55,7 @@ def main():
 	parser.add_argument("--input", help="file input", type=str, required=True)
 	parser.add_argument("--output", help="file output", type=str, required=True)
 	parser.add_argument("--strings", help="syntax = palavra|palavra2", type=str, required=True)
+	parser.add_argument("--extensions", help="extensions = palavra|palavra2", type=str)
 
 	args = parser.parse_args()
 
